9cb0f66ea2e19020aae85f020aa5190217cfbe65
azizkale <azizkale@hotmail.com>
2023-05-10 16:49:02 +0200

    fix/create-group

--- src/routes/group/groupcontrollers.ts
+++ src/routes/group/groupcontrollers.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import { Group } from "../../models/Group";
+import admin from 'firebase-admin'
+
 const { v1: uuidv1 } = require('uuid');
 
 const instanceGroup = new Group(null, null)
 
 const createGroup = async (req: Request, res: Response) => {
-    const { groupName, mentorId } = req.body;
+    const { groupName, mentorEmail } = req.body;
+    let mentorId;
+    admin.auth().getUserByEmail(mentorEmail)
+        .then((mentor) => {
+            mentorId = mentor.uid
+        })
     const groupId = await uuidv1();
     await instanceGroup.createGroup(groupName, mentorId, groupId).then((result: any) => {
         res.status(200).send(result)
