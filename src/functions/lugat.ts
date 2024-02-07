baf6d8192b2c34aa3112183db22caeb78e66fb46
azizkale <azizkale@hotmail.com>
2024-02-07 20:44:35 +0100

    lugata updated

--- src/functions/lugat.ts
+++ src/functions/lugat.ts
@@ -1,16 +1,43 @@
-const axios = require("axios");
+import axios from "axios";
+import { Request, Response } from "express";
 
-export const lugat = async () => {
-  console.log("1");
-  axios
-    .get(
-      "https://lugat.osmanlica.online/?kelime=vabeste&kaynak=browser&sadecehattikuran=false&filitre=Luggat,arap%C3%A7a%20kelimeler&manadaara=false&json=True"
-    )
-    .then((response: any) => {
-      console.log("2");
-      console.log(response.data);
-    })
-    .catch((error: any) => {
-      console.error(`Error: ${error.message}`);
+import { interval } from "rxjs";
+import { take } from "rxjs/operators";
+import * as fs from "fs";
+const text =
+  "Öyle ki, bu engin hazlarla coşan namaz kahramanı, doyma bilmeyen bir hisle, kemmiyet ve keyfiyet sınırlarının üstünde, niyetle derinleştirip sonsuzlaştırdığı; yakîniyle Hak’la irtibatlandırıp hulûsuyla ebedîleştirdiği, mal, can ve bütün ilâhî mevhibeler adına Hakk’a karşı minnet borcunu edaya yönelir; gönlünün bütün duyarlılığıyla Allah’ı anar ve inler.. Nebî’yi yâd eder, içi inşirahla dolar.. kendisiyle aynı mutluluğu paylaşan insanları düşünür, hayır dualarıyla gürler.. ve tekbirlerle başlattığı bu miraç yolculuğunu, dinin temeli sayılan şehadetlerle noktalar";
+
+export const lugat = async (req: Request, res: Response) => {
+  const words = text.split(" ");
+  let arr: string[] = [];
+
+  interval(3000)
+    .pipe(take(words.length))
+    .subscribe((index) => {
+      const word = words[index];
+      console.log(word);
+      axios
+        .get(
+          "https://lugat.osmanlica.online/?kelime=" +
+            word +
+            "&kaynak=browser&sadecehattikuran=false&filitre=Luggat,arap%C3%A7a%20kelimeler&manadaara=false&json=True"
+        )
+        .then((response: any) => {
+          if (response.data ?? false) {
+            arr.push(response.data);
+            console.log(response.data);
+            const filePath = "file.txt";
+
+            const dataString = response.data.join("\n");
+
+            // Append data to the file
+            // fs.appendFileSync(filePath, dataString + "\n");
+          } else {
+            console.log(word + " kelimesinin anlamı bulunamadı");
+          }
+        })
+        .catch((error: any) => {
+          console.error(`Error: ${error.message}`);
+        });
     });
 };
