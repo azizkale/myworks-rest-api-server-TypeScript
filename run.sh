5a52d6e18c30fde3ed9b519ebb81c8282374de28
azizkale <azizkale@hotmail.com>
2024-02-19 16:10:58 +0100

    run.sh update

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "start"
+
+echo "Starting the build and deployment process..."
 
 # Build the Docker image
 docker build -t easyreadrestful . || { echo "Docker build failed"; exit 1; }
@@ -7,7 +8,10 @@ docker build -t easyreadrestful . || { echo "Docker build failed"; exit 1; }
 # Stop the existing container if it's running
 docker stop easyreadrestful || true
 
+# Remove the existing container
+docker rm easyreadrestful || true
+
 # Run the Docker container
 docker run --name easyreadrestful -d -p 3000:3000 --rm easyreadrestful:latest || { echo "Docker run failed"; exit 1; }
 
-echo "end"
+echo "Build and deployment completed successfully."
