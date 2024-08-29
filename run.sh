d739cf2e8615de7512d49b151a88f3ea16ba0df2
azizkale <azizkale@hotmail.com>
2024-08-29 13:51:20 +0200

    run.sh updated

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 
 echo "Starting the build and deployment process..."
 
+# from TypeScript to JavaScript
+npm install
+npm run build
+
 docker build -t easyreadrestful . || true
-
 docker stop easyreadrestful_con || true
-
+docker rm easyreadrestful_con || true
 
 docker run --name easyreadrestful_con -d -p 3001:3001 --rm easyreadrestful:latest
 
