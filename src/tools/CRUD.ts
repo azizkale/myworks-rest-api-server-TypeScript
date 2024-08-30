531962452f25a1cc7657db3732ad3ad8ebc15753
azizkale <azizkale@hotmail.com>
2024-08-30 18:12:39 +0200

    single db

--- src/tools/CRUD.ts
+++ src/tools/CRUD.ts
@@ -1,45 +1,45 @@
-import { Databases, getDatabase } from "./DB";
+// import { getDatabase } from "./DB";
 
-export class CRUD {
-  public static create = async (db: Databases, path: string, data: any) => {
-    const database = getDatabase(db);
-    try {
-      await database.ref(path).set(data);
-      console.log("Data created successfully at", path);
-    } catch (error) {
-      console.error("Error creating data:", error);
-    }
-  };
+// export class CRUD {
+//   public static create = async (db: Databases, path: string, data: any) => {
+//     const database = getDatabase(db);
+//     try {
+//       await database.ref(path).set(data);
+//       console.log("Data created successfully at", path);
+//     } catch (error) {
+//       console.error("Error creating data:", error);
+//     }
+//   };
 
-  public static read = async (db: Databases, path: string) => {
-    const database = getDatabase(db);
-    try {
-      const snapshot = await database.ref(path).once("value");
-      const data = snapshot.val();
-      console.log("Data read successfully from", path, ":", data);
-      return data;
-    } catch (error) {
-      console.error("Error reading data:", error);
-    }
-  };
+//   public static read = async (db: Databases, path: string) => {
+//     const database = getDatabase(db);
+//     try {
+//       const snapshot = await database.ref(path).once("value");
+//       const data = snapshot.val();
+//       console.log("Data read successfully from", path, ":", data);
+//       return data;
+//     } catch (error) {
+//       console.error("Error reading data:", error);
+//     }
+//   };
 
-  public static update = async (db: Databases, path: string, data: any) => {
-    const database = getDatabase(db);
-    try {
-      await database.ref(path).update(data);
-      console.log("Data updated successfully at", path);
-    } catch (error) {
-      console.error("Error updating data:", error);
-    }
-  };
+//   public static update = async (db: Databases, path: string, data: any) => {
+//     const database = getDatabase(db);
+//     try {
+//       await database.ref(path).update(data);
+//       console.log("Data updated successfully at", path);
+//     } catch (error) {
+//       console.error("Error updating data:", error);
+//     }
+//   };
 
-  public static delete = async (db: Databases, path: string) => {
-    const database = getDatabase(db);
-    try {
-      await database.ref(path).remove();
-      console.log("Data deleted successfully from", path);
-    } catch (error) {
-      console.error("Error deleting data:", error);
-    }
-  };
-}
+//   public static delete = async (db: Databases, path: string) => {
+//     const database = getDatabase(db);
+//     try {
+//       await database.ref(path).remove();
+//       console.log("Data deleted successfully from", path);
+//     } catch (error) {
+//       console.error("Error deleting data:", error);
+//     }
+//   };
+// }
--- src/tools/DB.ts
+++ src/tools/DB.ts
@@ -1,16 +1,7 @@
-import { db_de, db_pir } from "./firebaseAdminInitialization";
+import { db_pir } from "./firebaseAdminInitialization";
 import * as admin from "firebase-admin";
 
-export enum Databases {
-  DB_pir = "DB_pir",
-  DB_de = "DB_de",
-}
-
-const databases: { [key in Databases]: admin.database.Database } = {
-  [Databases.DB_pir]: db_pir,
-  [Databases.DB_de]: db_de,
-};
-
-export const getDatabase = (db: Databases): admin.database.Database => {
-  return databases[db];
+// Single database export
+export const getDatabase = (): admin.database.Database => {
+  return db_pir;
 };
