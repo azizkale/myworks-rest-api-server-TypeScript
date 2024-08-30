6d82ded7abee279930dab22347c2bb550389d50e
azizkale <azizkale@hotmail.com>
2024-08-30 11:12:50 +0200

    dotenv file updated

--- run.sh
+++ run.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# echo "Starting the build and deployment process..."
+echo "Starting the build and deployment process..."
 
-# sudo docker build -t easyreadrestful . || true
-# sudo docker stop easyreadrestful_con || true
-# sudo docker rm easyreadrestful_con || true
+docker build -t easyreadrestful . || true
+docker stop easyreadrestful_con || true
+docker rm easyreadrestful_con || true
 
-# sudo docker run --name easyreadrestful_con -d -p 3001:3001 --rm easyreadrestful:latest
-
-# echo "Build and deployment completed successfully."
-#!/bin/bash
-
-echo "Starting the build and deployment process with Docker Compose..."
-
-# Projeyi Docker Compose ile build edin ve başlatın
-docker-compose up -d --build
+docker run --name easyreadrestful_con -d -p 3001:3001 --rm easyreadrestful:latest
 
 echo "Build and deployment completed successfully."
