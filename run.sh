89b16f6d72acd07f09c7bb73c3fdf6d2ea7c1fab
azizkale <azizkale@hotmail.com>
2024-08-30 09:48:45 +0200

    compose added

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Starting the build and deployment process..."
+# echo "Starting the build and deployment process..."
 
-sudo docker build -t easyreadrestful . || true
-sudo docker stop easyreadrestful_con || true
-sudo docker rm easyreadrestful_con || true
+# sudo docker build -t easyreadrestful . || true
+# sudo docker stop easyreadrestful_con || true
+# sudo docker rm easyreadrestful_con || true
 
-sudo docker run --name easyreadrestful_con -d -p 3001:3001 --rm easyreadrestful:latest
+# sudo docker run --name easyreadrestful_con -d -p 3001:3001 --rm easyreadrestful:latest
+
+# echo "Build and deployment completed successfully."
+#!/bin/bash
+
+echo "Starting the build and deployment process with Docker Compose..."
+
+# Projeyi Docker Compose ile build edin ve başlatın
+sudo docker-compose up -d --build
 
 echo "Build and deployment completed successfully."
